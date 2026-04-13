7c693d5ca4b330c17edcdf9df9eda163e14bcbbf
Tejas <pelab1.in@siemens.com>
2026-04-13 13:02:40 +0530

    Changes made

---
+++ CMakeFiles/ecommerce_store.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecommerce_store.dir/Customer.cpp.obj"
+  "CMakeFiles/ecommerce_store.dir/Customer.cpp.obj.d"
+  "CMakeFiles/ecommerce_store.dir/Order.cpp.obj"
+  "CMakeFiles/ecommerce_store.dir/Order.cpp.obj.d"
+  "CMakeFiles/ecommerce_store.dir/Product.cpp.obj"
+  "CMakeFiles/ecommerce_store.dir/Product.cpp.obj.d"
+  "CMakeFiles/ecommerce_store.dir/Store.cpp.obj"
+  "CMakeFiles/ecommerce_store.dir/Store.cpp.obj.d"
+  "CMakeFiles/ecommerce_store.dir/main.cpp.obj"
+  "CMakeFiles/ecommerce_store.dir/main.cpp.obj.d"
+  "ecommerce_store.exe"
+  "ecommerce_store.exe.manifest"
+  "ecommerce_store.pdb"
+  "libecommerce_store.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ecommerce_store.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
